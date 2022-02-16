733698a5b95261c8835ec909c5d84f9b2e441a98
Banuprakash <banuprakashc@yahoo.co.in>
2022-02-16 10:35:11 +0530

    day 3

--- README.md
+++ README.md
@@ -1442,7 +1442,75 @@ DELETE ==> DELETE
 
 
 
+```
+CustomersComponent.ts
 
+remove below code:
+  ngOnInit(): void {
+    this.customers = this.complete = [{
+      "id": 1,
+      "firstName": "Rachel",
+      "lastName": "Green ",
+      "gender": "female",
+      "address": "Blore"
+    },
+    {
+      "id": 2,
+      "firstName": "Chandler",
+      "lastName": "Bing",
+      "gender": "male",
+      "address": "West Street"
+    },
+    ....
+    ];
+  }
+
+
+New changes 
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import Customer from '../common/Customer';
+import { DataService } from '../common/data.service';
+
+@Component({
+  selector: 'app-customers',
+  templateUrl: './customers.component.html',
+  styleUrls: ['./customers.component.css']
+})
+export class CustomersComponent implements OnInit {
+  customers: Customer[] = [];
+  complete: Customer[] = [];
+  title: string = "Customer Application";
+  searchText: string = "";
+ 
+  // DI
+  constructor(private dataService: DataService) { 
+  }
+
+  ngOnInit(): void {
+    this.dataService.getCustomers().subscribe(data => {
+      this.customers = this.complete = data;
+    })
+  }
+
+  deleteCustomerData(id: number): void {
+    // this.customers = this.customers.filter(c => c.id !== id);
+    this.dataService.deleteCustomer(id).subscribe(data => console.log(data));
+  }
+
+  filterCustomers() {
+    this.customers = this.complete.filter(c => {
+      if ((c.firstName.toUpperCase().indexOf(this.searchText.toUpperCase()) >= 0)
+        ||
+        (c.lastName.toUpperCase().indexOf(this.searchText.toUpperCase()) >= 0)) {
+        return true;
+      }
+      return false;
+    });
+  }
+}
+
+```
 
 
 
--- customerapp/src/app/customers/customers.component.ts
+++ customerapp/src/app/customers/customers.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import Customer from '../common/Customer';
+import { DataService } from '../common/data.service';
 
 @Component({
   selector: 'app-customers',
@@ -10,57 +12,21 @@ export class CustomersComponent implements OnInit {
   customers: Customer[] = [];
   complete: Customer[] = [];
   title: string = "Customer Application";
-  searchText: string = "Hello";
-  constructor() { }
+  searchText: string = "";
+ 
+  // DI
+  constructor(private dataService: DataService) { 
+  }
 
   ngOnInit(): void {
-    this.customers = this.complete = [{
-      "id": 1,
-      "firstName": "Rachel",
-      "lastName": "Green ",
-      "gender": "female",
-      "address": "Blore"
-    },
-    {
-      "id": 2,
-      "firstName": "Chandler",
-      "lastName": "Bing",
-      "gender": "male",
-      "address": "West Street"
-    },
-    {
-      "id": 3,
-      "firstName": "Joey",
-      "lastName": "Tribbiani",
-      "gender": "male",
-      "address": "Kattegat"
-    },
-    {
-      "id": 4,
-      "firstName": "Monica",
-      "lastName": "Geller",
-      "gender": "female",
-      "address": "some address"
-    },
-    {
-      "id": 5,
-      "firstName": "Ross",
-      "lastName": "Geller",
-      "gender": "male",
-      "address": "some address "
-    },
-    {
-      "id": 6,
-      "firstName": "Phoebe",
-      "lastName": "Buffay",
-      "gender": "female",
-      "address": "some address"
-    }
-    ];
+    this.dataService.getCustomers().subscribe(data => {
+      this.customers = this.complete = data;
+    })
   }
 
   deleteCustomerData(id: number): void {
-    this.customers = this.customers.filter(c => c.id !== id);
+    // this.customers = this.customers.filter(c => c.id !== id);
+    this.dataService.deleteCustomer(id).subscribe(data => console.log(data));
   }
 
   filterCustomers() {
