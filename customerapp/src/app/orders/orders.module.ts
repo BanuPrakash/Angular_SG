c017bc802c6be5c489664252508698eeb3dd1341
Banuprakash <banuprakashc@yahoo.co.in>
2022-02-16 14:28:52 +0530

    day 3

--- customerapp/src/app/orders/orders.module.ts
+++ customerapp/src/app/orders/orders.module.ts
@@ -2,14 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrdersComponent } from './orders.component';
 
+import {RouterModule, Route} from '@angular/router';
 
+
+// configure Routes
+// http://localhost:4200/orders
+// http://localhost:4200/orders/customer/banu@gmail
+const routes:Route[] = [
+  {
+    path:'',
+    component:OrdersComponent
+  } 
+];
 
 @NgModule({
   declarations: [
     OrdersComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,  RouterModule.forChild(routes)
   ]
 })
 export class OrdersModule { }
